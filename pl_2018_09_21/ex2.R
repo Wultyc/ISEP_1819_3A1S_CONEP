31a67c444d2e85c2a54642cddb3b909b1bd3ea71
Aluno ISEP <Aluno ISEP>
2018-09-21 09:47:46 +0100

    ex2 - Inicio ex2

---
+++ pl_2018_09_21/ex2.R
@@ -0,0 +1,8 @@
+#a. Crie um vector apenas com a informação da precipitação (precipitacao1) 
+precipitacao1 = read.delim("precipitacao.txt")
+
+#b. Crie um vector, precipitação2, por eliminação das componentes NA do vector precipitacao1. 
+#c. Crie um vector lógico que indica as componentes do vector precipitacao2 com valores superiores a 1000 mm. 
+#d. Crie um vector que contenha apenas informação relativa aos anos em que a precipitação foi superior a 1000 mm.
+#e. Calcule a precipitação média e a respectiva variância. 
+#f. Represente graficamente dos dados [sugestão: use as funções plot, hist, boxplot.
