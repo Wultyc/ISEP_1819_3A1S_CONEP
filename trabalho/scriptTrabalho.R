cbc0434cc8038a87f445c5adecaeea4b4aabe62f
Aluno ISEP <Aluno ISEP>
2018-11-02 09:48:59 +0000

    media das medias

--- trabalho/scriptTrabalho.R
+++ trabalho/scriptTrabalho.R
@@ -11,13 +11,16 @@ library(readxl)
 
 #dados guardados no excel
 dados_trabalho = read_excel("dados_trabalho.xlsx")
+amplitude.linha <- apply(dados_trabalho[,2:4], 1, max) - apply(dados_trabalho[,2:4], 1, min)
 #View(dados_trabalho)
+#View(amplitude.linha)
 
 #Media de cada amostra
-media.linha <- apply(dados_trabalho[,2:4], 1, mean)
+media.linha <- apply(dados_trabalho[1:50,2:4], 1, mean)
 #View(media.linha)
 
 #Media Geral
-media.global <- mean(media.linha)
+media.global <- mean(unlist(media.linha))
+media.global
 
 
