b749de3e4858ed97e7cdbebd414dac74d788fba1
Nuno Dinis <1161042@isep.ipp.pt>
2018-11-09 09:06:25 +0000

    Carta de Controlo 1 e grafico

--- trabalho/scriptTrabalho.R
+++ trabalho/scriptTrabalho.R
@@ -11,9 +11,7 @@ library(readxl)
 
 #dados guardados no excel
 dados_trabalho = read_excel("dados_trabalho.xlsx")
-amplitude.linha <- apply(dados_trabalho[,2:4], 1, max) - apply(dados_trabalho[,2:4], 1, min)
 #View(dados_trabalho)
-#View(amplitude.linha)
 
 #Media de cada amostra
 media.linha <- apply(dados_trabalho[1:50,2:4], 1, mean)
@@ -22,5 +20,20 @@ media.linha <- apply(dados_trabalho[1:50,2:4], 1, mean)
 #Media Geral
 media.global <- mean(unlist(media.linha))
 media.global
+
+#Numero de amostras
+N <- NROW(media.linha)
+
+
+LCL <- 6
+UCL <- 8
+CL <- 7
+
+plot(media.linha, title="Carta de Controlo", xlab = "Numero da Amostra", ylab = "Espessura do Vidro", ylim=c(LCL-0.5, UCL+0.5))
+lines(rep(UCL,N), col="red")
+lines(rep(LCL,N), col="red")
+lines(rep(CL,N), col="blue")
+
+ 
 
 
